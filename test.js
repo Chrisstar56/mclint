f21610d7116af0d906bc97a129dab0899e68c8d7
Chrisstar56 <18631757+Chrisstar56@users.noreply.github.com>
2019-06-05 12:46:24 +0200

    test.js Yes

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-console.log({
-  key1: "value",
-  key2: 51,
-  key3: new Date(),
-  key4: ["12","23","24"]
-});
+var mclint = require('./parse');
+console.log("Command");
+//console.log(mclint.parseCommand("execute as @e"));
+console.log("directory");
+//mclint.parseDirectory("./test/Tinkery/", function(err, res){console.log(res)});
+console.log("file");
+mclint.parseFile("C:/Users/bboet/Desktop/Programmieren/NodeJS/mclintt/Tinkery/data/tinkery/functions/hello.mcfunction", function(err, res){console.log(err ,res});
